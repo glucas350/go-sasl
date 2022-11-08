05de84936f6faffa4349c1dca6c0fb164fc6341e
George Lucas <glucas@aethon.com>
2022-11-08 12:37:13 -0500

    update xoauth

--- xoauth2.go
+++ xoauth2.go
@@ -1,48 +1,62 @@
 package sasl
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 )
 
 // The XOAUTH2 mechanism name.
-const Xoauth2 = "XOAUTH2"
+const XOAuth2 = "XOAUTH2"
 
-// An XOAUTH2 error.
-type Xoauth2Error struct {
+type XOAuth2Error struct {
 	Status  string `json:"status"`
 	Schemes string `json:"schemes"`
 	Scope   string `json:"scope"`
 }
 
-// Implements error.
-func (err *Xoauth2Error) Error() string {
-	return fmt.Sprintf("XOAUTH2 authentication error (%v)", err.Status)
+type XOAuth2Options struct {
+	Username     string
+	AccessToken  string
+	RefreshToken string
+}
+
+func OAuth2String(user, accessToken string) string {
+	return "user=" + user + "\001auth=Bearer " + accessToken + "\001\001"
+}
+
+func XOAuth2String(user, accessToken string) string {
+	s := OAuth2String(user, accessToken)
+	if false {
+		return base64.StdEncoding.EncodeToString([]byte(s))
+	}
+	return s
+}
+
+func (err *XOAuth2Error) Error() string {
+	return fmt.Sprintf(XOAuth2+" authentication error (%v)", err.Status)
 }
 
 type xoauth2Client struct {
-	Username string
-	Token    string
+	XOAuth2Options
 }
 
 func (a *xoauth2Client) Start() (mech string, ir []byte, err error) {
-	mech = Xoauth2
-	ir = []byte("user=" + a.Username + "\x01auth=Bearer " + a.Token + "\x01\x01")
-	return
+	mech = XOAuth2
+	ir = []byte(XOAuth2String(a.Username, a.AccessToken))
+	return mech, ir, nil
 }
 
 func (a *xoauth2Client) Next(challenge []byte) ([]byte, error) {
-	// Server sent an error response
-	xoauth2Err := &Xoauth2Error{}
-	if err := json.Unmarshal(challenge, xoauth2Err); err != nil {
-		return nil, err
-	} else {
-		return nil, xoauth2Err
+	authBearerErr := &XOAuth2Error{}
+	if err := json.Unmarshal(challenge, authBearerErr); err != nil {
+		return nil, fmt.Errorf("unmarshal %s: %w", challenge, err)
 	}
+	return nil, fmt.Errorf("%s: %w", challenge, authBearerErr)
 }
 
-// An implementation of the XOAUTH2 authentication mechanism, as
-// described in https://developers.google.com/gmail/xoauth2_protocol.
-func NewXoauth2Client(username, token string) Client {
-	return &xoauth2Client{username, token}
+// An implementation of the OAUTHBEARER authentication mechanism, as
+// described in RFC 7628.
+func NewXOAuth2Client(opt *XOAuth2Options) *xoauth2Client {
+	return &xoauth2Client{*opt}
 }
